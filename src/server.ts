5ac4dc36137e25a7e79b437980a6cab598d421f2
Nicolas <nicolaspierina@gmai.com>
2025-12-07 04:47:48 -0300

    ajuste de cors

--- src/server.ts
+++ src/server.ts
@@ -1,11 +1,26 @@
-import express from 'express';
-import 'dotenv/config';
-import routes from './routes';
+import express from "express";
+import "dotenv/config";
+import cors from "cors";
+import routes from "./routes";
 
 const app = express();
 
+// --- CORS CONFIG ---
+app.use(
+  cors({
+    origin: [
+      "http://localhost:5173",
+      "https://stunning-tulumba-25517c.netlify.app",
+    ],
+    methods: ["GET", "POST", "PUT", "DELETE"],
+    allowedHeaders: ["Content-Type", "Authorization"],
+  })
+);
+
+// --- JSON ---
 app.use(express.json());
 
+// --- ROUTES ---
 app.use(routes);
 
 const PORT = process.env.PORT || 3000;
