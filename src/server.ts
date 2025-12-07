47c09f7581400ca4809ba2bf1a2dae1af8f05839
Nicolas <nicolaspierina@gmai.com>
2025-12-07 12:34:59 -0300

    teste bcrypt

--- src/server.ts
+++ src/server.ts
@@ -2,6 +2,7 @@ import express from "express";
 import "dotenv/config";
 import cors from "cors";
 import routes from "./routes";
+import bcrypt from "bcryptjs";
 
 const app = express();
 
@@ -29,8 +30,23 @@ app.listen(PORT, () => {
   console.log(`Server is running on http://localhost:${PORT}`);
 });
 
+// TESTES DE DEPLOY REMOVER DEPOIS
 app.get("/test-env", (req, res) => {
   res.json({
     JWT_SECRET: process.env.JWT_SECRET ? "LOADED" : "MISSING",
+  });
+});
+
+app.get("/bcrypt-test", async (req, res) => {
+  const testPassword = "123";
+  const storedHash =
+    "$2a$10$y3jQnJPwmETxAf.Dl7EtFObK8u0UqjhMT8GR42kUZjP1k2XHvUSeG";
+
+  const ok = await bcrypt.compare(testPassword, storedHash);
+
+  res.json({
+    password: testPassword,
+    hash: storedHash,
+    matches: ok,
   });
 });
